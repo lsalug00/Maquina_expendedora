3a94888d4361f7268ab1aae8066a972b68ae6afa
lsalug00 <lidiasaludes@gmail.com>
2022-11-02 10:50:30 +0100

    Metodo getTotalDineroAcumulado

--- MaquinaExpendedoraSimple.java
+++ MaquinaExpendedoraSimple.java
@@ -46,6 +46,10 @@ public class MaquinaExpendedoraSimple {
         return balanceClienteActual;
     }
 
+    public int getTotalDineroAcumulado(){
+        return totalDineroAcumulado;
+    }
+    
     /**
      * Simula la introduccion de dinero por parte del cliente actual
      */
